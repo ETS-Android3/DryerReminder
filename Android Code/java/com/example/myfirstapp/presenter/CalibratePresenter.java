5f45deb74bd698833562be1c51da42afe54445ff
Mmohler1 <71154838+Mmohler1@users.noreply.github.com>
2022-01-26 10:36:29 -0700

    Add files via upload

--- Android Code/java/com/example/myfirstapp/presenter/CalibratePresenter.java
+++ Android Code/java/com/example/myfirstapp/presenter/CalibratePresenter.java
@@ -1,6 +1,7 @@
 package com.example.myfirstapp.presenter;
 
 import android.os.StrictMode;
+import android.widget.Toast;
 
 import com.example.myfirstapp.model.AxesModel;
 import com.example.myfirstapp.model.ClientModel;
@@ -15,64 +16,72 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 
-
+/**
+ * Presenter for the Calibrate Fragment class. Acts like a controller to the Fragment's View.
+ * Calls the API from the Raspberry Pi to get calibration data from the accelerometer.
+ */
 public class CalibratePresenter implements CalibrateContract.Presenter {
     public CalibrateContract.View view;
     private AxesModel savedAxes;
 
+    //No idea might delete
     public CalibratePresenter(CalibrateContract.View view)
     {
         this.view = view;
     }
 
-    //Call the pi and get the axes models. Save it to text file
+    /**
+     * Void method that calls the Calibrate API on the Pi and receive a JSON of the AxesModel.
+     * The Model will then be saved to the phone using a text file or something similar.
+     *
+     */
     public void doCalibrate()
     {
+        //Model that holds the information to pull data from the Client.
         ClientModel piModel = new ClientModel();
 
-        int SDK_INT = android.os.Build.VERSION.SDK_INT;
-        if (SDK_INT > 8) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                    .permitAll().build();
-            StrictMode.setThreadPolicy(policy);
 
-            //Above needed for get to work
-            System.out.println("START OF THE CLICK--------------------------------------");
-            //Setup OkHttpClient
-            OkHttpClient client = new OkHttpClient();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
-            //Setup the builder so that it does not time out after 10 seconds (Needed for dryer
-            //I believe write is the part that needs to wait awhile.
-            OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            builder.connectTimeout(2, TimeUnit.HOURS) // connect timeout
-                    .writeTimeout(2, TimeUnit.HOURS) // write timeout
-                    .readTimeout(2, TimeUnit.HOURS); // read timeout
+        //Above needed for get to work
+        System.out.println("START OF THE CLICK--------------------------------------");
 
-            client = builder.build();
+        //Setup OkHttpClient
+        OkHttpClient client = new OkHttpClient();
 
-            //For empty post
-            RequestBody formBody = new FormBody.Builder()
-                    .build();
+        //Determines how long the client will try to connect to the URL before timing out.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(2, TimeUnit.MINUTES) // connect timeout
+                .writeTimeout(2, TimeUnit.MINUTES) // write timeout
+                .readTimeout(2, TimeUnit.MINUTES); // read timeout
 
-            //Setup the request and pick the URL we are calling
-            Request postRequest = new Request.Builder()
-                    .post(formBody)
-                    .url("http://" + piModel.getIpAddress() + ":" + piModel.getPort()+"/DryPi/calibrate")  //api that is being called
-                    .addHeader("cache-control", "no-cache")
-                    .addHeader("Authorization", "Bearer " + piModel.getToken())
-                    .build();
-            try {
-                System.out.println("START OF END--------------------------------------");
-                Response response = client.newCall(postRequest).execute();
-                System.out.println(response.body().string());
+        client = builder.build();
 
-                System.out.println("END OF THE CLICK--------------------------------------");
-            } catch (IOException e) {
-                System.out.println("Start OF THE CLICK BAD---------------------------------");
-                e.printStackTrace();
+        //Needed to post, even from empty form
+        RequestBody formBody = new FormBody.Builder()
+                .build();
 
-            }
+        //Setup the request and pick the URL we are calling
+        Request postRequest = new Request.Builder()
+                .post(formBody)
+                .url("http://" + piModel.getIpAddress() + ":" + piModel.getPort()+"/DryPi/calibrate")  //api that is being called
+                .addHeader("cache-control", "no-cache")
+                .addHeader("Authorization", "Bearer " + piModel.getToken()) //Security Token
+                .build();
+        try {
+            System.out.println("START OF END--------------------------------------");
+            Response response = client.newCall(postRequest).execute();
+            System.out.println(response.body().string());
+
+            System.out.println("END OF THE CLICK--------------------------------------");
+        } catch (IOException e) {
+            System.out.println("Start OF THE CLICK BAD---------------------------------");
+            e.printStackTrace();
+
         }
+
     }
 
 }
