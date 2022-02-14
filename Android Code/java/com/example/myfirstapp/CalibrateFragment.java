e4f7c5807436c1609d1094b1978a0c17c9da44f5
Mmohler1 <71154838+Mmohler1@users.noreply.github.com>
2022-02-14 10:35:46 -0700

    Add files via upload

--- Android Code/java/com/example/myfirstapp/CalibrateFragment.java
+++ Android Code/java/com/example/myfirstapp/CalibrateFragment.java
@@ -1,13 +1,22 @@
 package com.example.myfirstapp;
 
-import android.content.Context;
-import android.content.Intent;
+
 import android.os.Bundle;
 
+
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
+
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+
 import androidx.navigation.fragment.NavHostFragment;
+
+import androidx.work.ExistingWorkPolicy;
+
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkContinuation;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,19 +25,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.myfirstapp.background.ClientWorker;
 import com.example.myfirstapp.databinding.FragmentCalibrateBinding;
-import com.example.myfirstapp.model.AxesModel;
-import com.example.myfirstapp.model.ClientModel;
+
 import com.example.myfirstapp.presenter.CalibrateContract;
 import com.example.myfirstapp.presenter.CalibratePresenter;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+
+import java.util.List;
+
 
 
 /**
@@ -41,10 +46,17 @@ public class CalibrateFragment extends Fragment implements CalibrateContract.Vie
     //Variables
     private FragmentCalibrateBinding binding;
     TextView showBasicText;
-    boolean isBusy; //Will use this in the feature to keep the user form pressing buttons
+    Button calibrateButton;
+    Button backButton;
 
     //Call the presenter
     private CalibrateContract.Presenter presenter;
+
+    //Call Worker
+    private WorkManager myClientManager;
+    private LiveData<List<WorkInfo>> mySavedWorkerInfo;
+
+    private boolean firstCall; //Checks if the device has been called
 
     /**
      * Setup the Calibrate page with the layout, view, view group, etc.
@@ -60,14 +72,19 @@ public class CalibrateFragment extends Fragment implements CalibrateContract.Vie
             Bundle savedInstanceState)
     {
 
+        //Set to true when user first comes to page
+        firstCall = true;
 
-        isBusy = false;
+        //Initialize WorkerManager and WorkerInfo
+        myClientManager = WorkManager.getInstance(getActivity().getApplicationContext());
+        mySavedWorkerInfo = myClientManager.getWorkInfosByTagLiveData("Calibrate");
 
 
+
+
+        //Binding Fragment
         binding = FragmentCalibrateBinding.inflate(inflater, container, false);
-
         showBasicText = binding.getRoot().findViewById(R.id.calibrate_text_view1);
-
         return binding.getRoot();
 
 
@@ -83,11 +100,8 @@ public class CalibrateFragment extends Fragment implements CalibrateContract.Vie
     {
         super.onViewCreated(view, savedInstanceState);
 
-        //Instantiate the presenter
-        presenter = new CalibratePresenter(this);
-
-        Button calibrateButton = view.findViewById(R.id.calibrate_device_button);
-        Button backButton = view.findViewById(R.id.calibrate_back_button);
+        calibrateButton = view.findViewById(R.id.calibrate_device_button);
+        backButton = view.findViewById(R.id.calibrate_back_button);
 
         //Confirm the user wants to call the API to have the Pi Calibrate and send the data back to the phone.
         calibrateButton.setOnClickListener(new View.OnClickListener()
@@ -97,36 +111,29 @@ public class CalibrateFragment extends Fragment implements CalibrateContract.Vie
             @Override
             public void onClick(View view)
             {
+                //Set to false so device can show in progress to user
+                firstCall = false;
 
-                //calibrateButton.setVisibility(View.INVISIBLE);
-               // backButton.setEnabled(false);
-
-                //Checks to see if device is busy. If not then set to true until method is finished.
-
-                    //Attempt to connect to pi and save information to text file.
-                    try
-                    {
+                //Attempt to connect to pi and save information to text file.
+                try
+                {
 
                     //If the version of Android is too old, then prevent the button from running.
                     int SDK_INT = android.os.Build.VERSION.SDK_INT;
                     if (SDK_INT > 8)
                     {
                         System.out.println("----------------------------Start");
-                        //Call the presenter to open a connection the pi
 
-                        //Run calibrate and save number. Anything other then 0 is an error
-                        int errorNumber = presenter.doCalibrate();
 
-                        //Change text base
-                        if (errorNumber == 0)
-                        {
-                            showBasicText.setText("Calibration has finished");
+                        OneTimeWorkRequest calibrateWorker = new OneTimeWorkRequest.Builder(ClientWorker.class)
+                                .addTag("Calibrate")
+                                .build();
 
-                        }
-                        else
-                        {
-                            showBasicText.setText("Calibration Failed. Make sure device is on.");
-                        }
+                        WorkContinuation start = myClientManager.beginUniqueWork("stuff", ExistingWorkPolicy.KEEP, calibrateWorker);
+
+                        start.enqueue();
+
+
 
                         System.out.println("----------------------------END");
                     }
@@ -161,6 +168,45 @@ public class CalibrateFragment extends Fragment implements CalibrateContract.Vie
 
             }
         });
+
+
+        //Will show the status when the background task is called
+        mySavedWorkerInfo.observe(getViewLifecycleOwner(), listOfWorkInfos -> {
+
+            //If work is null or empty then do nothing.
+            if (listOfWorkInfos == null || listOfWorkInfos.isEmpty())
+            {
+                return;
+            }
+
+            //The Tagged LiveData will get called here
+            WorkInfo workInfo = listOfWorkInfos.get(0);
+
+            //LiveData persists after first called. This keeps it from updating until getting called first.
+            if (!firstCall)
+            {
+                //Check if background task is finished.
+                boolean finished = workInfo.getState().isFinished();
+                if (!finished)
+                {
+                    //Change texts and buttons to show calibrate is in progress
+                    showInProgress();
+                }
+                else
+                {
+                    //Change text and buttons to show calibrate has finished.
+                    showFinishedProgress();
+                }
+
+                //Clear work info list to reset data if button is pressed again
+                listOfWorkInfos.clear();
+            }
+
+
+        });
+
+
+
     }
 
     /**
@@ -170,7 +216,9 @@ public class CalibrateFragment extends Fragment implements CalibrateContract.Vie
     public void onDestroyView()
     {
         super.onDestroyView();
+        mySavedWorkerInfo.removeObservers(getViewLifecycleOwner());
         binding = null;
+
     }
 
 
@@ -185,75 +233,46 @@ public class CalibrateFragment extends Fragment implements CalibrateContract.Vie
         myToast.show();
     }
 
+    /**
+     * Visually change the buttons, text, and anything else on the screen
+     * to show that the phone is trying to calibrate the device.
+     */
+    public void showInProgress()
+    {
+        //Text Changes
+        showBasicText.setText("        Device is calibrating  \nDO NOT TOUCH THE DEVICE!");
+
+        //Might make text color turn more grey and change failed to this red
+        //Calibrate Button Changes
+        calibrateButton.setText("Calibrating");
+        calibrateButton.setBackgroundColor(getResources().getColor(R.color.red_grey));
+        calibrateButton.setEnabled(false);
+
+        //Back Button Changes
+        backButton.setEnabled(false);
+        backButton.setBackgroundColor(getResources().getColor(R.color.red_grey));
+    }
 
     /**
-     * Take Calibrated range from the Raspberry Pi and save it to a text file to use it later.
-     * Currently reads the same text file to show it has been created. That will be moved to Dryer Fragment later
-     * @param savedRange Save range that was calibrated from pi
-     *
+     * Visually change the buttons, text, and anything else on the screen
+     * to show that the phone has finished calibrating.
      */
-    @Override
-    public void writeToFile(AxesModel savedRange)
+    public void showFinishedProgress()
     {
-        System.out.println("Write to File-----------------------");
-        //Try to write to a text file
-        try
-        {
-            //Write saved range to config Calibrate text file
-            String filename = "configCalibrate.txt";
-            String fileContents = (savedRange.getAxisX() + "\n" + savedRange.getAxisY() + "\n" + savedRange.getAxisZ() + "\n");
+        //Text Changes
+        showBasicText.setText("Calibration has finished");
 
-            try (FileOutputStream fileOutput = getContext().openFileOutput(filename, Context.MODE_PRIVATE))
-            {
-                fileOutput.write(fileContents.getBytes(StandardCharsets.UTF_8));
-            }
+        //Calibrate Button Changes
+        calibrateButton.setText("Calibrate Again");
+        calibrateButton.setBackgroundColor(getResources().getColor(R.color.green_grey));
+        calibrateButton.setEnabled(true);
 
-        }
-        catch(IOException e)
-        {
-            System.out.println(e);
-        }
-
-        //Reading the file Move to Dryer Later
-        //Converts text file numbers to axes model
-        AxesModel newRange = new AxesModel();
-
-        //Create the file input stream
-        FileInputStream fileInput = null;
-
-        //Try to find text file
-        try
-        {
-            fileInput = getContext().openFileInput("configCalibrate.txt"); //Use context to read from text file
-        }
-        catch (FileNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-
-        //Take the text file and try to convert it to an AxesModel
-        InputStreamReader inputStreamReader = new InputStreamReader(fileInput, StandardCharsets.UTF_8);
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(inputStreamReader))
-        {
-            String line = reader.readLine();
-
-            //Convert the range to an axesModel
-            newRange.setAxisX((Double.parseDouble(line)));
-            line = reader.readLine();
-            newRange.setAxisY((Double.parseDouble(line)));
-            line = reader.readLine();
-            newRange.setAxisZ((Double.parseDouble(line)));
-
-            String contents = stringBuilder.toString();
-            System.out.println("File Found:" + newRange.getAxisX() + " " + newRange.getAxisY() + " " + newRange.getAxisZ());
-        }
-        catch (IOException e)
-        {
-            System.out.println(e);
-        }
-
+        //Back Button Changes
+        backButton.setEnabled(true);
+        backButton.setBackgroundColor(getResources().getColor(R.color.green_grey));
     }
+
+
 
 
 }
