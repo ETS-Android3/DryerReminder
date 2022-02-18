f07d7598a54939266c7add42408775b2a87ee4bf
Mmohler1 <71154838+Mmohler1@users.noreply.github.com>
2022-02-17 18:35:36 -0700

    Add files via upload

---
+++ Android Code/java/com/example/myfirstapp/background/AdjustWorker.java
@@ -0,0 +1,141 @@
+package com.example.myfirstapp.background;
+
+import android.content.Context;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+
+import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.myfirstapp.model.AxesModel;
+import com.example.myfirstapp.model.ClientModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class AdjustWorker extends Worker
+{
+    private AxesModel savedAxes = new AxesModel();
+    int adjustNumber;
+
+    public AdjustWorker(@NonNull Context context, @NonNull WorkerParameters workerParams)
+    {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork()
+    {
+
+        //Pull number from Adjust Fragment
+        adjustNumber = getInputData().getInt("adjustSeekbar", 3);
+        System.out.println(adjustNumber);
+
+        //Call connect to client
+        int errorNumber = connectToClient();
+        System.out.println("Work Error Number: " + errorNumber);
+
+        Data adjustOutput = new Data.Builder()
+                .putInt("adjustOutput", errorNumber)
+                .build();
+
+        if (errorNumber == 0)
+        {
+            return Result.success(adjustOutput);
+        }
+        else if (errorNumber == 1)
+        {
+            return Result.failure(adjustOutput);
+        }
+        else
+        {
+            return Result.failure(adjustOutput);
+        }
+
+    }
+
+
+    int connectToClient()
+    {
+
+        //Setup Json String
+        String json = "{\"adjust\":" + adjustNumber + "}";
+        System.out.print(json);
+
+        //Model that holds the information to pull data from the Client.
+        ClientModel piModel = new ClientModel();
+
+        //Needed to call the Pi
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+        System.out.println("START OF THE CLICK--------------------------------------");
+
+        //Setup OkHttpClient
+        OkHttpClient client = new OkHttpClient();
+
+        //Determines how long the client will try to connect to the URL before timing out.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(1, TimeUnit.MINUTES) // connect timeout
+                .writeTimeout(1, TimeUnit.MINUTES) // write timeout
+                .readTimeout(1, TimeUnit.MINUTES); // read timeout
+
+        client = builder.build();
+
+        MediaType JSON = MediaType.get("application/json; charset=utf-8");
+
+        //IT BETTER WORK
+        RequestBody body = RequestBody.Companion.create(json, JSON);
+
+        //Setup the request and pick the URL we are calling
+        Request postRequest = new Request.Builder()
+                .post(body)
+                .url("http://" + piModel.getIpAddress() + ":" + piModel.getPort()+"/DryPi/adjust")  //api that is being called
+                .addHeader("cache-control", "no-cache")
+                .addHeader("Authorization", "Bearer " + piModel.getToken()) //Security Token
+                .build();
+        try
+        {
+            System.out.println("START OF END--------------------------------------");
+
+            //Execute call.
+            Response response = client.newCall(postRequest).execute();
+
+            //Take the response and save it to a string.
+            String jsonString = response.body().string();
+            System.out.println(jsonString);
+
+            System.out.println("END OF THE CLICK--------------------------------------");
+
+            //If the API does not return Adjust, return 0
+            if(jsonString.equals("Adjust"))
+            {
+                return 0;
+            }
+
+            //If response is not adjust then assume an error.
+            return 1;
+        }
+        catch (IOException e)
+        {
+            System.out.println("Bad CLICK---------------------------------");
+            e.printStackTrace();
+            return 2;
+        }
+    }
+}
---
+++ Android Code/java/com/example/myfirstapp/background/CalibrateWorker.java
@@ -0,0 +1,196 @@
+package com.example.myfirstapp.background;
+
+import android.content.Context;
+import android.os.StrictMode;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.myfirstapp.model.AxesModel;
+import com.example.myfirstapp.model.ClientModel;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Client worker that will
+ */
+public class CalibrateWorker extends Worker
+{
+
+    private AxesModel savedAxes = new AxesModel();
+
+
+    public CalibrateWorker(@NonNull Context context, @NonNull WorkerParameters workerParams)
+    {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork()
+    {
+
+        if (connectToClient() == 0)
+        {
+            return Result.success();
+        }
+        else
+        {
+            return Result.failure();
+        }
+
+    }
+
+
+    int connectToClient()
+    {
+        //Model that holds the information to pull data from the Client.
+        ClientModel piModel = new ClientModel();
+
+        //Needed to call the Pi
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+        System.out.println("START OF THE CLICK--------------------------------------");
+
+        //Setup OkHttpClient
+        OkHttpClient client = new OkHttpClient();
+
+        //Determines how long the client will try to connect to the URL before timing out.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(1, TimeUnit.MINUTES) // connect timeout
+                .writeTimeout(1, TimeUnit.MINUTES) // write timeout
+                .readTimeout(1, TimeUnit.MINUTES); // read timeout
+
+        client = builder.build();
+
+        //Needed to post, even from empty form
+        RequestBody formBody = new FormBody.Builder()
+                .build();
+
+        //Setup the request and pick the URL we are calling
+        Request postRequest = new Request.Builder()
+                .post(formBody)
+                .url("http://" + piModel.getIpAddress() + ":" + piModel.getPort()+"/DryPi/calibrate")  //api that is being called
+                .addHeader("cache-control", "no-cache")
+                .addHeader("Authorization", "Bearer " + piModel.getToken()) //Security Token
+                .build();
+        try
+        {
+            System.out.println("START OF END--------------------------------------");
+
+            //Execute call.
+            Response response = client.newCall(postRequest).execute();
+
+            //Take the response and save it to a string.
+            String jsonString = response.body().string();
+            //System.out.println(jsonString);
+
+            //Convert the string to a JSON
+            JSONObject json = new JSONObject(jsonString);
+
+            //Convert JSON Object to the AxesModel
+            savedAxes = new AxesModel(json.getDouble("axisX"), json.getDouble("axisY"), json.getDouble("axisZ"));
+
+            //Call the view to save the axes to a text file.
+            writeToFile(savedAxes);
+
+
+            System.out.println("END OF THE CLICK--------------------------------------");
+            return 0;
+        }
+        catch (IOException | JSONException e)
+        {
+            System.out.println("Bad CLICK---------------------------------");
+            e.printStackTrace();
+            return 1;
+        }
+    }
+
+    /**
+     * Take Calibrated range from the Raspberry Pi and save it to a text file to use it later.
+     * Currently reads the same text file to show it has been created. That will be moved to Dryer Fragment later
+     * @param savedRange Save range that was calibrated from pi
+     *
+     */
+    public void writeToFile(AxesModel savedRange)
+    {
+        System.out.println("Write to File-----------------------");
+        //Try to write to a text file
+        try
+        {
+            //Write saved range to config Calibrate text file
+            String filename = "configCalibrate.txt";
+            String fileContents = (savedRange.getAxisX() + "\n" + savedRange.getAxisY() + "\n" + savedRange.getAxisZ() + "\n");
+
+            try (FileOutputStream fileOutput = getApplicationContext().openFileOutput(filename, Context.MODE_PRIVATE))
+            {
+                fileOutput.write(fileContents.getBytes(StandardCharsets.UTF_8));
+            }
+
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+
+        //Reading the file Move to Dryer Later------------------------------
+        //Converts text file numbers to axes model
+        AxesModel newRange = new AxesModel();
+
+        //Create the file input stream
+        FileInputStream fileInput = null;
+
+        //Try to find text file
+        try
+        {
+            fileInput = getApplicationContext().openFileInput("configCalibrate.txt"); //Use context to read from text file
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+        //Take the text file and try to convert it to an AxesModel
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInput, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(inputStreamReader))
+        {
+            String line = reader.readLine();
+
+            //Convert the range to an axesModel
+            newRange.setAxisX((Double.parseDouble(line)));
+            line = reader.readLine();
+            newRange.setAxisY((Double.parseDouble(line)));
+            line = reader.readLine();
+            newRange.setAxisZ((Double.parseDouble(line)));
+
+            String contents = stringBuilder.toString();
+            System.out.println("File Found:" + newRange.getAxisX() + " " + newRange.getAxisY() + " " + newRange.getAxisZ());
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
+
+    }
+}
